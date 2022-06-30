859a15f9bbc1a8ca3556617258d5cf6d37b63e34
wenxich <wenxi.c.chen@gmail.com>
2022-06-30 09:42:10 -0400

    Added style changes.

--- js/divscroll.js
+++
@@ -1,8 +0,0 @@
-$(document).on('click','.searchbychar', function(event) {
-    event.stopPropagation()
-    event.preventDefault();
-    var target = "#" + this.getAttribute('data-target');
-    $('html, body').animate({
-        scrollTop: $(target).offset().top
-    }, 2000);
-});
