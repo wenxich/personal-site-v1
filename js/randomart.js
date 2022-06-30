242f97ac0a5a8b50a23cc3dc3ddfecd30910410f
wenxich <wenxi.c.chen@gmail.com>
2022-06-30 12:39:05 -0400

    Trying to position divs.

--- js/randomart.js
+++ js/randomart.js
@@ -1,11 +1,16 @@
-document.getElementById('title').addEventListener('click', function(e) {
+document.getElementById('title').addEventListener('click', function() {
 
-    var img = document.createElement('img');
+    var x = event.clientX;
+    var y = event.clientY;
+    var snowball = document.createElement('img');
 
     img.setAttribute('src','https://wenxi.carrd.co/assets/images/gallery07/3dea9f56.jpg?v=5d09a7fc');
     img.setAttribute('width','100px');
 
-    e.target.appendChild(img);
+    snowball.style.display = '';
+    snowball.style.position = 'absolute';
+    snowball.style.left = x + 'px';
+    snowball.style.top = y + 'px';
 });
 
 function artimg() {
