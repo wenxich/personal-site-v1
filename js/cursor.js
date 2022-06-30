a3285ac1bc95c27ac3913b2023d9d6cd858eb9dc
wenxich <wenxi.c.chen@gmail.com>
2022-06-30 10:31:53 -0400

    Added style changes.

--- js/cursor.js
+++ js/cursor.js
@@ -1,8 +1,41 @@
-var $cursor = $('.cursor');
-function moveCursor(e) {
-    $cursor.css({
-        "top": e.pageY,
-        "left": e.pageX
-    });
-}
- $(window).on('mousemove', moveCursor);
+document.addEventListener("DOMContentLoaded", function(event) {
+    var cursor = document.querySelector(".custom-cursor");
+    var links = document.querySelectorAll("a");
+    var initCursor = false;
+
+    for (var i = 0; i < links.length; i++) {
+        var selfLink = links[i];
+
+        selfLink.addEventListener("mouseover", function() {
+            cursor.classList.add("custom-cursor--link");
+        });
+        selfLink.addEventListener("mouseout", function() {
+            cursor.classList.remove("custom-cursor--link");
+        });
+    }
+
+    window.onmousemove = function(e) {
+        var mouseX = e.clientX;
+        var mouseY = e.clientY;
+
+        if (!initCursor) {
+            // cursor.style.opacity = 1;
+            TweenLite.to(cursor, 0.3, {
+                opacity: 1
+            });
+            initCursor = true;
+        }
+
+        TweenLite.to(cursor, 0, {
+            top: mouseY + "px",
+            left: mouseX + "px"
+        });
+    };
+
+    window.onmouseout = function(e) {
+        TweenLite.to(cursor, 0.3, {
+            opacity: 0
+        });
+        initCursor = false;
+    };
+});
