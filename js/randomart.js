41115cb63a13ccce465852c8305fce8e9dc79783
wenxich <wenxi.c.chen@gmail.com>
2022-06-30 12:30:45 -0400

    Trying to position divs.

---
+++ js/randomart.js
@@ -0,0 +1,13 @@
+document.getElementById('title').addEventListener('click', function(e) {
+
+    var img = document.createElement('img');
+
+    img.setAttribute('src','https://wenxi.carrd.co/assets/images/gallery07/3dea9f56.jpg?v=5d09a7fc');
+    img.setAttribute('width','100px');
+
+    e.target.appendChild(img);
+});
+
+function artimg() {
+    //i'll write this later, test it first
+}
