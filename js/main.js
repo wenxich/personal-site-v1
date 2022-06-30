7678cc32b2c312b2c8cb30dd8c32d2d9bc3583b6
wenxich <wenxi.c.chen@gmail.com>
2022-06-30 09:11:50 -0400

    Added style changes.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ function scrollFunction() {
         document.getElementById("navbar").style.top = "-50px";
     }
 }
+
+//jQuery for scrolling to div
+
+$(document).on('click','.searchbychar', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var target = "#" + this.getAttribute('data-target');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 2000);
+});
